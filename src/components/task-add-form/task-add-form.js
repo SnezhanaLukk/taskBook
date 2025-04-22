e7a2b4c2d44cc788a11e06b6b870afc356f98460
SnezhanaLukk <lukkarevasa@gmail.com>
2025-04-22 11:02:45 +0300

    the form dsts is loaded into the state and component parameter

--- src/components/task-add-form/task-add-form.js
+++ src/components/task-add-form/task-add-form.js
@@ -1,17 +1,45 @@
+import { Component } from 'react';
 import './task-add-form.css';
 
-const TaskAddForm = () => {
-    return (
-        <div className='app-add-form'>
-            <h3>Добавить новую задачу</h3>
-            <form action="" className='add-form d-flex'>
-                <input type="text" className='form-control nem-post-label' placeholder='Новая задача' />
-                <input type="date" className='form-control nem-post-label' placeholder='Дедлайн' />
-                <button type='submit' className='btn btn-outline-light'>Добавить</button>
-            </form>
+class TaskAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: '',
+            deadline: ''
+        }
+    }
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    render() {
+        const { task, deadline } = this.state;
+        return (
+            <div className='app-add-form'>
+                <h3>Добавить новую задачу</h3>
+                <form action="" className='add-form d-flex'>
+                    <input
+                        onChange={this.onValueChange}
+                        name="task"
+                        value={task}
+                        type="text"
+                        className='form-control nem-post-label'
+                        placeholder='Новая задача' />
+                    <input
+                        onChange={this.onValueChange}
+                        name="deadline"
+                        value={deadline}
+                        type="date"
+                        className='form-control nem-post-label'
+                        placeholder='Дедлайн' />
+                    <button type='submit' className='btn btn-outline-light'>Добавить</button>
+                </form>
 
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default TaskAddForm;
